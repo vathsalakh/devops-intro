2367e1b1a34e64f9a60d4bc147f105c1f7bde188
vathsalakh <vathsala.kh@gmail.com>
2025-06-05 11:20:25 -0600

    lint error

--- src/sum.ts
+++ src/sum.ts
@@ -1,5 +1,8 @@
-function sum(a:number ,b:number): number {
-  return a + b;
+var unused = "this will cause a lint error";
+
+function sum(A:number ,b:number): number {
+    let x = 10;
+    return  A + b;
 }
 
 console.log('Sum of 3 and 5 is:', sum(3, 5));
